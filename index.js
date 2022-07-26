6cac3e2b384f777fc3a598730b021b5a90f1e21b
Rab Beverly <rab@robertbeverly.com>
2022-07-26 11:14:53 -0400

    added a favoritething

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.get('/api-kiddies', (req, res) => {
   res.send(JSON.stringify({what: "My api", why: "because I can", examples: "https://github.com/rbeverly/api-kiddies", favoritethings: ["raindrops", "roses", "whiskers", "kittens"]}));
 });
 
+app.post('/api-kiddies', (req, res) => {
+  res.send(JSON.stringify({what: "My api", why: "because I can", examples: "https://github.com/rbeverly/api-kiddies", favoritethings: ["raindrops", "roses", "whiskers", "kittens", "flag{posting-to-apis}"]}));
+});
+
 app.listen(5100, () => {
   console.log("Running on port 5100.");
 });
